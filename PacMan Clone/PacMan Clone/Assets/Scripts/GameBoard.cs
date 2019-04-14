1ae3218be8a7d1d0ad8fecd2dc030f910a74f451
Dorota <dorota.win96@gmail.com>
2019-04-14 13:26:10 +0200

    duszki dwa

---
+++ PacMan Clone/PacMan Clone/Assets/Scripts/GameBoard.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoard : MonoBehaviour
+{
+	private static int boardWidth = 28;
+	private static int boardHeight = 36;
+
+	public int totalPellets = 0;
+	public int score = 0;
+
+
+	public GameObject[,] board = new GameObject[boardWidth, boardHeight];
+
+	void Start()
+	{
+		Object[] objects = GameObject.FindObjectsOfType(typeof(GameObject));
+		foreach (GameObject o in objects)
+		{
+			Vector2 pos = o.transform.position;
+			if(o.name != "PacMan" && o.name != "Nodes" && o.name != "NonNodes" && o.name != "Maze" && o.name!= "Pellets" && o.tag!= "Ghost")
+			{
+
+				if (o.GetComponent<Tile> () != null)
+				{
+					if (o.GetComponent<Tile>().isPellet || o.GetComponent<Tile>().isSuperPellet)
+					{
+						totalPellets++;
+					}
+				}
+
+				board[(int)pos.x, (int)pos.y] = o;
+			}else
+			{
+				Debug.Log("Found PacMan at: " + pos);
+			}
+		}
+	}
+
+
+	void Update()
+	{
+
+	}
+}
